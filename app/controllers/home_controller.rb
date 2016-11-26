c4ec1805edb84a1ed97827e2eb8b9c695fb69478
bc-alfreid <alfreid.pascual@bigcommerce.com>
2016-11-26 23:38:24 +1100

    tabify search results, styling update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,15 +7,16 @@ class HomeController < ApplicationController
     @run_registrations = current_user.run_registrations
     @run_requests = current_user.run_requests
 
-    # @hash = Gmaps4rails.build_markers(@run_registrations) do |run_registration, marker|
-    #   marker.lat run_registration.latitude
-    #   marker.lng run_registration.longitude
-    #   marker.infowindow run_registration.desciption    # creates info on the marker
-    # end
-
     if !current_user.profile
       redirect_to new_profile_path
     end
-  end
 
+    @dic_run_registrations
+    @run_registrations.each do |run_registration|
+      p '...................'
+      p run_registration.run_registration_requests
+      p run_registration.run_registration_requests.count
+    end
+
+  end
 end
